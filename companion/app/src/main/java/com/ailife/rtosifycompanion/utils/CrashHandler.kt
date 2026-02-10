8fbf8a622d94820d2da7d3a8c8ee96f504728e44
ailife8881 <ailife8881@gmail.com>
2026-02-10 21:14:46 +0800

    save detail to crash log

--- companion/app/src/main/java/com/ailife/rtosifycompanion/utils/CrashHandler.kt
+++ companion/app/src/main/java/com/ailife/rtosifycompanion/utils/CrashHandler.kt
@@ -1,6 +1,8 @@
 package com.ailife.rtosifycompanion.utils
 
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import java.io.File
 import java.io.FileOutputStream
@@ -25,8 +27,24 @@ class CrashHandler(private val context: Context) : Thread.UncaughtExceptionHandl
 
     private fun handleException(e: Throwable) {
         try {
-            val timestamp = SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US).format(Date())
-            val fileName = "crash_log_$timestamp.txt"
+            val date = Date()
+            val timestamp = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(date)
+            val fileTimestamp = SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US).format(date)
+            val fileName = "crash_log_$fileTimestamp.txt"
+
+            val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                context.packageManager.getPackageInfo(context.packageName, PackageManager.PackageInfoFlags.of(0))
+            } else {
+                @Suppress("DEPRECATION")
+                context.packageManager.getPackageInfo(context.packageName, 0)
+            }
+            val versionName = packageInfo.versionName
+            val versionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                packageInfo.longVersionCode
+            } else {
+                @Suppress("DEPRECATION")
+                packageInfo.versionCode.toLong()
+            }
             
             // Get the external files directory
             val logDir = context.getExternalFilesDir(null)
@@ -39,12 +57,16 @@ class CrashHandler(private val context: Context) : Thread.UncaughtExceptionHandl
             
             val stringWriter = StringWriter()
             val printWriter = PrintWriter(stringWriter)
+            
+            printWriter.println("App Version: $versionName ($versionCode)")
+            printWriter.println("Crash Time: $timestamp")
+            printWriter.println("---------- Stack Trace ----------")
             e.printStackTrace(printWriter)
-            val stackTrace = stringWriter.toString()
+            val logContent = stringWriter.toString()
             printWriter.close()
 
             FileOutputStream(logFile).use { fos ->
-                fos.write(stackTrace.toByteArray())
+                fos.write(logContent.toByteArray())
             }
             
             Log.d("CrashHandler", "Crash log saved to ${logFile.absolutePath}")
